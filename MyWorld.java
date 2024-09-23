208b44f6019b7a5da2853812489ddf7d3134defe
KarthikRaveeswaran <348899022@gapps.yrdsb.ca>
2024-09-23 13:25:50 -0400

    Label and movement code

--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    public int score = 0;
+    Label scoreLabel;
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -19,6 +21,28 @@ public class MyWorld extends World
         super(600, 400, 1);
         
         Elephant elephant = new Elephant();
+<<<<<<< Updated upstream
         addObject(elephant, 300,300);
+=======
+        addObject(elephant, 300,200);
+        
+        Label scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        createApple();
+    }
+    
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
+>>>>>>> Stashed changes
     }
 }
